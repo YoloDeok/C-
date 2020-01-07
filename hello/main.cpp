8b3770e16c13facac6bc1d9fd07615316e248ebf
yolodeok <yolodeok@gmail.com>
2020-01-07 14:07:01 +0900

    code environment change

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+//#include <iostream>
 #include <stdio.h>
+#include "test.h"
 
 int main(){
+    int temp_val;
+    temp_val = add(3,5);
+    printf("temp%d\n",temp_val);
 
     printf("helloc\n");
     printf("hellocpp\n");
