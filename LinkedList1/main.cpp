805dc5df2de91c30d5e62fbe9dd77c98e4b30288
yolodeok <yolodeok@gmail.com>
2020-01-11 15:49:16 +0900

    add dummy based linkedlist1

--- LinkedList1/main.cpp
+++ LinkedList1/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
 
-#if 1
+#if 0
 //simple linkedlist ver 00, head to tail based linkedlist
     Node * head = NULL;
     Node * tail = NULL;
@@ -77,7 +77,84 @@ int main(){
             
 
 
-#elif 0
+#elif 1 
+//dummy node based linkedlist
+
+    Node * head = NULL;
+    Node * tail = NULL;
+    Node * curr = NULL;
+
+    printf("if you exit the program, enther the -1\n");
+
+    head = (Node *)malloc(sizeof(Node)); // add dummy node
+    tail = head;
+
+    while(1){
+        int temp_input_val = 0; 
+        printf("input_number: ");
+        scanf("%d",&temp_input_val);
+
+        if(temp_input_val == -1){
+            break;
+        }
+        else{
+            
+            Node *NewNode = (Node*)malloc(sizeof(Node));
+            NewNode->data = temp_input_val;
+            NewNode->next = NULL;
+            /*
+            if(head == NULL){
+                head = NewNode;
+                tail = NewNode;
+            }
+            else{
+                tail->next = NewNode;
+                tail = NewNode;
+            }
+            */
+           tail->next = NewNode;
+           tail = NewNode;
+        }
+    }
+        printf("\n");
+
+            if(tail == head){
+                return 0;
+            }
+            else{
+                printf("printf numbers\n");
+                curr = head;
+                //printf("input:%d", curr->data);
+                printf("input:");
+                while(curr->next != NULL){
+                    curr = curr->next;
+                    printf(" %d,", curr->data);
+                }
+                printf("\n");
+            }
+
+        printf("\n");
+        printf("free memory\n");
+
+            if(tail == head){
+                return 0;
+            }
+            else
+            {
+                Node *DelNode = head;
+                Node *DelNodeNext = head->next;
+                //printf("delete:%d",head->data);
+                //free(DelNode);
+                printf("delete:");
+
+                while(DelNodeNext != NULL){
+                    DelNode = DelNodeNext;
+                    DelNodeNext = DelNode->next;
+                    printf(" %d,",DelNode->data);
+                    free(DelNode);
+                }
+                printf("\n");
+            }
 
 #endif
 
